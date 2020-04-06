b637db7f61a90eb3d5efc71c27bd7b7a875ec653
IdionKim <ksundong@gmail.com>
2020-04-06 21:37:18 +0900

    [#3] chore: Spring Project 시작
    - spring boot version 2.2.6
    - spring data jdbc
    - spring web
    - spring boot devtools
    - h2 database
    - mysql connector

---
+++ BE/README.md
@@ -0,0 +1,9 @@
+# TODO Project Team 9 BackEnd
+
+## Local
+
+- h2 database 사용하여 빠른 개발
+
+## Deploy
+
+- MySQL Database 사용
---
+++ BE/src/main/java/kr/codesquad/todo9/Todo9Application.java
@@ -0,0 +1,13 @@
+package kr.codesquad.todo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Todo9Application.class, args);
+    }
+
+}
