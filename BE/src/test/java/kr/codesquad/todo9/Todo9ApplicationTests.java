de03c526ad2a18178cfc0927b60bb27732a07245
IdionKim <ksundong@gmail.com>
2020-04-06 21:37:18 +0900

    [#3] chore: Spring Project 시작
    - spring boot version 2.2.6
    - spring data jdbc
    - spring web
    - spring boot devtools
    - h2 database
    - mysql connector

---
+++ BE/src/test/java/kr/codesquad/todo9/Todo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad.todo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todo9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
