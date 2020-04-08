10c3da1c0d309588ad7d594c08579e412142cdae
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-09 00:58:26 +0900

    [#46] refactor: 불필요한 코드 삭제, 불필요한 파일 삭제
    * index.ts 에서 불필요한 코드 삭제
    * Column.ts 및 Modal.ts 파일 삭제

--- FE/src/ts/index.ts
+++ FE/src/ts/index.ts
@@ -1,43 +1,5 @@
-//import '../style/reset.css';
+import '../style/reset.css';
 import '../style/style.css';
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    dispatcher.on
-});
-
-/* dispatcher.ts */
-
-class Dispatcher {    
-    on<
-        MessageType extends keyof AppMessageMap
-    >(
-        messageType: MessageType,
-        handler: (message: AppMessageMap[MessageType] & {messageType: MessageType}) => void
-    ): void { }
-}
-
-/* messages.ts */
-interface AddCommentMessage {
-    commentId: number;
-    comment: string;
-    userId: number;
-}
-
-interface PostPictureMessage {
-    pictureId: number;
-    userId: number;
-}
-
-interface AppMessageMap {
-    "ADD_COMMENT": AddCommentMessage,
-    "POST_PICTURE": PostPictureMessage
-}
-type AppMessage = AppMessageMap[keyof AppMessageMap];
-
-/* app.ts */
-const dispatcher = new Dispatcher();
-
-
-dispatcher.on("ADD_COMMENT", (message) => {
-    console.log(message.comment);
 });
