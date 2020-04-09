1d9b7167470e51db66143ca97ca5b59a71593e74
choisohyun <sohyun4283@naver.com>
2020-04-09 21:21:58 +0900

    chore: 개발자 모드 설정 추가
    - 추후 프로덕션 모드도 추가할 예정임

--- FE/webpack.config.js
+++ FE/webpack.config.js
@@ -1,80 +1,75 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/ts/index.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: './'
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all",
-                },
-            },
+  mode: "development",
+  entry: "./src/ts/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "./",
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
         },
+      },
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-typescript',
-                        ],
-                        plugins: [
-                            '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-proposal-object-rest-spread'
-                        ]
-                    }
-                }
-            },
-            // {
-            //     test: /\.(css)$/,
-            //     exclude: /node_modules/,
-            //     use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
-            // },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //       'css-modules-typescript-loader',
-            //       {
-            //         loader: 'css-loader',
-            //         options: {
-            //           modules: true
-            //         }
-            //       }
-            //     ]
-            // }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/view/index.html'
-        }),
-        // new MiniCssExtractPlugin({
-        //     filename: '[name].css',
-        //     chunkFilename: '[name].[id].css'
-        //   })
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-object-rest-spread",
+            ],
+          },
+        },
+      },
+      // {
+      //     test: /\.(css)$/,
+      //     exclude: /node_modules/,
+      //     use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
+      // },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // {
+      //     test: /\.css$/,
+      //     use: [
+      //       'css-modules-typescript-loader',
+      //       {
+      //         loader: 'css-loader',
+      //         options: {
+      //           modules: true
+      //         }
+      //       }
+      //     ]
+      // }
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/view/index.html",
+    }),
+    // new MiniCssExtractPlugin({
+    //     filename: '[name].css',
+    //     chunkFilename: '[name].[id].css'
+    //   })
+  ],
 };
