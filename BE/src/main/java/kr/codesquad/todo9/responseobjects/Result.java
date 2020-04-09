6d83e8f5b8d9002db797a18386bc17b78a39b21d
IdionKim <ksundong@gmail.com>
2020-04-09 22:05:59 +0900

    [#24] feat: login API 구현
    POST 요청으로 login을 호출하면 cookie로 jwt를 저장하도록 합니다.
    Close #24 Issue.

---
+++ BE/src/main/java/kr/codesquad/todo9/responseobjects/Result.java
@@ -0,0 +1,19 @@
+package kr.codesquad.todo9.responseobjects;
+
+public class Result {
+    private boolean result;
+    private String message;
+
+    public Result(boolean result, String message) {
+        this.result = result;
+        this.message = message;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
