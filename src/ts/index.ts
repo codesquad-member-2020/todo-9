efeab1330b01c1ca2ed9ef4e927f6d7d523f966e
choisohyun <sohyun4283@naver.com>
2020-04-09 21:26:16 +0900

    [#62] feat: 데이터 받아와 랜더링하는 코드 추가
    - 초기 랜더 이후 데이터를 fetch로 받아와 랜더를 해주고, 이벤트를 등록하도록
    비동기적인 코드를 작성함

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,15 @@
-import '../style/reset.css';
+import "../style/reset.css";
 
-import Column from './column'
-import EditNote from './editnote'
-import EditColumn from './editcolumn'
-import Activity from './activity'
+import Column from "./column";
+import EditNote from "./editnote";
+import EditColumn from "./editcolumn";
+import Activity from "./activity";
 
-import {qs$} from './lib/util'
-import fetchRequest from './common/fetchRequest'
+import { qs$ } from "./lib/util";
+import fetchRequest from "./common/fetchRequest";
+import { SERVICE_URL, INIT_DATA_URI } from "./common/constants";
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
   const activity = new Activity();
   const editNote = new EditNote(activity);
   const editColumn = new EditColumn(activity);
@@ -18,28 +19,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
     1. render
     Column, EditNote, EditColumn 의 render() 함수를 호출하여
     body 에 innerHTML 로 렌더링.
-
-    ex)
-    document.body.innerHTML = column.render() + editNote.render() + editColumn.render()
   */
+  document.body.innerHTML = column.render() + editNote.render() + editColumn.render();
 
   /*
     2. registerEventListender
 
-    column.registerEventListener();
     editNote.registerEventListener();
     editcolumn.registerEventListener();
-  */
-  
-   /*
+    */
+  // column.registerEventListener();
+
+  /*
     3. fetch
     BE 에 초기 데이테를 요청하여 Column 에 전달.
-
-    ex)
-    fetchRequest(SERVICE_URL.INFORMATION, "GET")
-    .then(response => response.json())
-    .then(data => {
-      column.receiveInitialData(data);
-    });
-  */
+    */
+  fetchRequest(SERVICE_URL + INIT_DATA_URI, "GET")
+    .then((response) => response.json())
+    .then((data) => column.receiveInitialData(data))
+    .then(() => column.registerEventListener());
 });
