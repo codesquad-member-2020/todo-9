640a7851a8d99a5efb4095a91d9e462c019163aa
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-10 12:23:49 +0900

    [#15] 카드 내용 수정 기능을 담당하는 Modal 기능 개발
    * save-button 클릭 시 textarea 의 글자를 card-content 에
      설정하도록 기능 구현
    
    * cancel-button 클릭 시 Modal 이 닫히도록 구현.
    
    * textarea 에서 input 발생 시 save-button 활성화/비활성화 기능 구현.

--- src/ts/editnote.ts
+++ src/ts/editnote.ts
@@ -1,24 +1,90 @@
 import '../style/edit-note.css';
 import Activity from './activity'
-import View from "./view"
+import Modal from './modal';
 
-class EditNote implements View{
+class EditNote extends Modal {
   private activity: Activity;
 
-  constructor(activity: Activity) {
+  constructor(activity: Activity, className: string) {
+    super(className);
     this.activity = activity;
   }
 
   render(): string {
-    return ``;
+    return `
+    <div class="edit-note-modal">
+      <div class="modal-container">
+        <div class="modal-header">
+          <div class="title-area">
+            <h1>Edit note</h1>  
+          </div>
+          <div class="button-area">
+            <button class="close-button">×</button>  
+          </div>
+        </div>
+        <div class="modal-body">
+          <h1>Note</h1>
+          <textarea class="text-area" autofocus></textarea>
+          <button class="save-button">Save note</button>
+        </div>
+      </div>
+    </div>
+    `;
   }
 
   registerEventListener(): void {
-    
+    const modal = document.querySelector('.' + this.className);
+
+    modal?.addEventListener('click', evt => {
+      const className = (<HTMLElement>evt.target).className;
+
+      switch (className) {
+        case 'save-button': {
+          this.saveButtonHandler(evt)
+          break;
+        }
+        case 'close-button': {
+          this.cancelButtonHandler(evt)
+          break;
+        }
+      }
+    });
+
+    modal?.addEventListener('input', evt => {
+      const className = (<HTMLElement>evt.target).className;
+
+      switch (className) {
+        case 'text-area': {
+          this.inputHandler(evt)
+          break;
+        }
+      }
+    });
   }
 
-  showModal(columnId: string, cardId: string, content: string, target: HTMLInputElement): void {
-    
+  saveButtonHandler(evt: Event): void {
+    const textElement = this.getTextElement();
+    this.currentEvent.innerHTML = textElement.value;
+
+    this.hideModal();
+  }
+
+  cancelButtonHandler(evt: Event): void {
+    this.hideModal();
+  }
+
+  inputHandler(evt: Event): void {
+    //Todo: 객체 탐색하는 코드 없애보기. (렌더링 후 캐싱 등을 통해서)
+    const modal = <Element>document.querySelector('.' + this.className);
+    const saveButton: any = <HTMLElement>modal?.querySelector('.save-button');
+    const length = (<HTMLInputElement>evt.target).value.length;
+
+    if (!length) {
+      saveButton.disabled = true;
+    }
+    else {
+      saveButton.disabled = false;
+    }
   }
 }
 
