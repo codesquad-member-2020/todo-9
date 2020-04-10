0f6347c9b2beec37133e5968f35cad90288a611e
IdionKim <ksundong@gmail.com>
2020-04-10 22:28:57 +0900

    [#67] refactor: Column 도메인 클래스 생성을 하면서 구현에 문제가 되는 부분 수정
    ColumnId를 ColumnKey로 변경하여 column이 card와 1:n 매핑이 될 수 있도록 수정하였습니다.

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -29,20 +29,20 @@ create table if not exists BOARD
 
 create table if not exists `COLUMN`
 (
-	ID bigint auto_increment,
-	NAME varchar(24) null,
-	CREATED_AT datetime not null default now(),
-	UPDATED_AT datetime not null default now(),
-	ARCHIVED_AT datetime null,
-	IS_ARCHIVED boolean not null default false,
-	`ORDER` int null,
-	BOARD_ID bigint null,
-	CRT_USER_ID bigint null,
-	UPD_USER_ID bigint null,
-	constraint COLUMN_pk
-		primary key (ID),
-	constraint COLUMN_BOARD_ID_fk
-		foreign key (BOARD_ID) references BOARD (ID),
+    ID           bigint auto_increment,
+    NAME         varchar(24) null,
+    CREATED_AT   datetime    not null default now(),
+    UPDATED_AT   datetime    not null default now(),
+    ARCHIVED_AT  datetime    null,
+    IS_ARCHIVED  boolean     not null default false,
+    COLUMN_ORDER int         null,
+    BOARD_ID     bigint      null,
+    CRT_USER_ID  bigint      null,
+    UPD_USER_ID  bigint      null,
+    constraint COLUMN_pk
+        primary key (ID),
+    constraint COLUMN_BOARD_ID_fk
+        foreign key (BOARD_ID) references BOARD (ID),
     constraint COLUMN_USER_ID_fk
         foreign key (CRT_USER_ID) references USER (ID),
     constraint COLUMN_USER_ID_fk_2
@@ -58,17 +58,17 @@ create table if not exists CARD
     ARCHIVED_AT datetime     null,
     IS_ARCHIVED boolean      not null default false,
     CARD_ORDER  int          null,
-    COLUMN_ID   bigint       null,
+    COLUMN_KEY  bigint       null,
     CRT_USER_ID bigint       null,
     UPD_USER_ID bigint       null,
     constraint CARD_pk
         primary key (ID),
     constraint CARD_COLUMN_ID_fk
-        foreign key (COLUMN_ID) references `COLUMN` (ID),
+        foreign key (COLUMN_KEY) references `COLUMN` (ID),
     constraint CARD_USER_ID_fk
         foreign key (CRT_USER_ID) references USER (ID),
     constraint CARD_USER_ID_fk_2
-		foreign key (UPD_USER_ID) references USER (ID)
+        foreign key (UPD_USER_ID) references USER (ID)
 );
 
 create table if not exists LOG
