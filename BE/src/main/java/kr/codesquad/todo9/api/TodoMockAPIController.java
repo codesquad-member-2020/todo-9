86aac09d4d12ff297b048c8a663d07e5de5bd782
IdionKim <ksundong@gmail.com>
2020-04-11 18:00:04 +0900

    [#66] refactor: 요청 경로 변경
    MockAPI와 API의 요청 경로를 서로 바꾸었습니다.
    이제 GET /board API가 실제 Data를 반환합니다.
    Close #66 Issue.

--- BE/src/main/java/kr/codesquad/todo9/api/TodoMockAPIController.java
+++ BE/src/main/java/kr/codesquad/todo9/api/TodoMockAPIController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/mock")
 public class TodoMockAPIController {
 
     private static final Logger log = LoggerFactory.getLogger(TodoMockAPIController.class);
