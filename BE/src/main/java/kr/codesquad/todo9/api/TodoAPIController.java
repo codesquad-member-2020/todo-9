3984a8b34cc2bef65ed4c1acf03a6ad46c636917
IdionKim <ksundong@gmail.com>
2020-04-11 18:00:04 +0900

    [#66] refactor: 요청 경로 변경
    MockAPI와 API의 요청 경로를 서로 바꾸었습니다.
    이제 GET /board API가 실제 Data를 반환합니다.
    Close #66 Issue.

--- BE/src/main/java/kr/codesquad/todo9/api/TodoAPIController.java
+++ BE/src/main/java/kr/codesquad/todo9/api/TodoAPIController.java
@@ -4,15 +4,22 @@ import kr.codesquad.todo9.domain.*;
 import kr.codesquad.todo9.repository.BoardRepository;
 import kr.codesquad.todo9.repository.UserRepository;
 import kr.codesquad.todo9.responseobjects.Result;
+import kr.codesquad.todo9.utils.JwtUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping("/mock")
 public class TodoAPIController {
 
     private static final Logger log = LoggerFactory.getLogger(TodoAPIController.class);
@@ -22,11 +29,24 @@ public class TodoAPIController {
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
 
-
     public TodoAPIController(UserRepository userRepository,
                              BoardRepository boardRepository) {
         this.userRepository = userRepository;
         this.boardRepository = boardRepository;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Result> login(HttpServletResponse httpServletResponse) {
+        Result result = new Result(true, "success");
+        User user = userRepository.findById(defaultUserId).orElseThrow(RuntimeException::new);
+        String jws = JwtUtils.createJws(user);
+        log.debug("user: {}", user);
+        log.debug("jws: {}", jws);
+
+        Cookie jwsCookie = new Cookie("jws", jws);
+        httpServletResponse.addCookie(jwsCookie);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/user/list")
