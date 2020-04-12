3a1a946827decbb4aef3659b5e2ca2fb8536d375
choisohyun <sohyun4283@naver.com>
2020-04-12 22:04:54 +0900

    [#82] chore: dev/prod 설정, dotenv 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,64 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  entry: "./src/ts/index.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "./",
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
+module.exports = (env, options) => {
+  const config = {
+    entry: "./src/ts/index.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "./",
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+          },
         },
       },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-typescript',
-            ],
-            plugins: [
-              '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-proposal-object-rest-spread'
-            ]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/view/index.html'
-    }),
-  ],
-};
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-typescript"],
+              plugins: [
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-proposal-object-rest-spread",
+              ],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+  };
 
+  if (options.mode === "development") {
+    config.plugins = [
+      new HtmlWebpackPlugin({
+        template: "./src/view/index.html",
+      }),
+      new Dotenv(),
+    ];
+  } else {
+    config.plugins = [new CleanWebpackPlugin(["dist"])];
+  }
+
+  return config;
+};
