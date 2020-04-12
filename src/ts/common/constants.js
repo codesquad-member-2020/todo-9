a39f421db0cfd89e7eec27986af711766464b4a6
choisohyun <sohyun4283@naver.com>
2020-04-12 21:00:02 +0900

    [#13] refactor: fetch 요청 메소드 상수화 코드 추가
    - 메소드가 하드코딩되는 것을 방지하기 위해 추가함

--- src/ts/common/constants.js
+++ src/ts/common/constants.js
@@ -1,3 +1,6 @@
-export const SERVICE_URL = "http://15.165.21.99/";
-export const INIT_DATA_URI = "api/board";
-export const CHANGE_DATA_URI = "";
+export const METHOD = {
+  POST: "POST",
+  GET: "GET",
+  PUT: "PUT",
+  DELETE: "DELETE",
+};
