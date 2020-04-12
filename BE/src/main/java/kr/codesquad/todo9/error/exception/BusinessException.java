cb0dc756807760ca838d2794bc3bb60a05d439a7
IdionKim <ksundong@gmail.com>
2020-04-12 20:50:04 +0900

    [#27] feat: BusinessException 클래스 생성
    - RuntimeException을 상속하는 클래스입니다.
    - 모든 비즈니스 로직에서 등장할 수 있는 Exception의 최상위 클래스입니다.

---
+++ BE/src/main/java/kr/codesquad/todo9/error/exception/BusinessException.java
@@ -0,0 +1,22 @@
+package kr.codesquad.todo9.error.exception;
+
+import kr.codesquad.todo9.error.ErrorCode;
+
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
