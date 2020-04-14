551d149e408fdcfb2263e587247d9ea6bea654bc
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-14 14:36:46 +0900

    [#108] fetch 기능 추가
    Delete, Edit 에 대한 Fetch 기능 추가

--- src/ts/common/configs.ts
+++ src/ts/common/configs.ts
@@ -1,5 +1,7 @@
 export const SERVICE_URL = "http://15.165.21.99/";
 export const INIT_DATA_URI = "api/board";
+export const EDIT_DATA_URI = "api/column/{boardKey}/card/{columnKey}"
+export const DELETE_DATA_URI = "api/column/{boardKey}/card/{columnKey}"
 export const CHANGE_DATA_URI = "";
 
 export const configs = {
