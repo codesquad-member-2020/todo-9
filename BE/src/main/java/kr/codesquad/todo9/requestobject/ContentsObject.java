f8e1caa2b77daef8b0bc961dcb661281aa53e04c
IdionKim <ksundong@gmail.com>
2020-04-15 01:17:52 +0900

    [#31] feat: Java Bean Validation을 ContentsObject에 적용
    이제 요청시에 맞지 않는 값일 경우 400 에러를 리턴합니다.
    상세한 에러로그를 포함하여 드립니다.
    Close #31 Issue.

--- BE/src/main/java/kr/codesquad/todo9/requestobject/ContentsObject.java
+++ BE/src/main/java/kr/codesquad/todo9/requestobject/ContentsObject.java
@@ -1,7 +1,10 @@
 package kr.codesquad.todo9.requestobject;
 
+import javax.validation.constraints.Size;
+
 public class ContentsObject {
 
+    @Size(max = 500)
     private String contents;
 
     public String getContents() {
