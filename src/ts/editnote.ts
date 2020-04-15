2c77248c0e375f6e40ac34b59807d039013c9b88
choisohyun <sohyun4283@naver.com>
2020-04-16 00:08:28 +0900

    [#126] fix: import된 configs 삭제

--- src/ts/editnote.ts
+++ src/ts/editnote.ts
@@ -1,8 +1,8 @@
-import '../style/edit-note.css';
-import Activity from './activity'
-import Modal from './modal';
+import "../style/edit-note.css";
+import Activity from "./activity";
+import Modal from "./modal";
 import fetchRequest from "./common/fetchRequest";
-import { configs, SERVICE_URL, INIT_DATA_URI, EDIT_DATA_URI } from "./common/configs";
+import { SERVICE_URL, INIT_DATA_URI, EDIT_DATA_URI } from "./common/configs";
 import { METHOD } from "./common/constants";
 
 class EditNote extends Modal {
@@ -36,29 +36,29 @@ class EditNote extends Modal {
   }
 
   registerEventListener(): void {
-    const modal = document.querySelector('.' + this.className);
+    const modal = document.querySelector("." + this.className);
 
-    modal?.addEventListener('click', evt => {
+    modal?.addEventListener("click", (evt) => {
       const className = (<HTMLElement>evt.target).className;
 
       switch (className) {
-        case 'save-button': {
-          this.saveButtonHandler(evt)
+        case "save-button": {
+          this.saveButtonHandler(evt);
           break;
         }
-        case 'close-button': {
-          this.cancelButtonHandler(evt)
+        case "close-button": {
+          this.cancelButtonHandler(evt);
           break;
         }
       }
     });
 
-    modal?.addEventListener('input', evt => {
+    modal?.addEventListener("input", (evt) => {
       const className = (<HTMLElement>evt.target).className;
 
       switch (className) {
-        case 'text-area': {
-          this.inputHandler(evt)
+        case "text-area": {
+          this.inputHandler(evt);
           break;
         }
       }
@@ -67,15 +67,13 @@ class EditNote extends Modal {
 
   requestEditCard(boardKey: string, columnKey: string, body: any) {
     let requestURI: string = <string>(SERVICE_URL + EDIT_DATA_URI);
-    const cvtURI = requestURI
-      .replace("{boardKey}", boardKey)
-      .replace("{columnKey}", columnKey);
+    const cvtURI = requestURI.replace("{boardKey}", boardKey).replace("{columnKey}", columnKey);
 
     fetchRequest(cvtURI, METHOD.PUT, body)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   }
 
   saveButtonHandler(evt: Event): void {
@@ -84,7 +82,7 @@ class EditNote extends Modal {
 
     const card: any = (<HTMLElement>this.currentTargetElement).closest(".card");
     const column: any = (<HTMLElement>this.currentTargetElement).closest(".column");
-    const body = {"contents": textElement.value};
+    const body = { contents: textElement.value };
 
     const columnKey = card.getAttribute("data-card-key");
     const boardKey = column.getAttribute("data-column-key");
@@ -99,14 +97,13 @@ class EditNote extends Modal {
 
   inputHandler(evt: Event): void {
     //Todo: 객체 탐색하는 코드 없애보기. (렌더링 후 캐싱 등을 통해서)
-    const modal = <Element>document.querySelector('.' + this.className);
-    const saveButton: any = <HTMLElement>modal?.querySelector('.save-button');
+    const modal = <Element>document.querySelector("." + this.className);
+    const saveButton: any = <HTMLElement>modal?.querySelector(".save-button");
     const length = (<HTMLInputElement>evt.target).value.length;
 
     if (!length) {
       saveButton.disabled = true;
-    }
-    else {
+    } else {
       saveButton.disabled = false;
     }
   }
