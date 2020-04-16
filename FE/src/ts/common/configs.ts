5025b383340f60715184c031c7484e8ce085426f
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-16 13:57:06 +0900

    [#141] chore: 로그 관련 동작 수정.
    * Add 기능 동작 시 서버로부터 response 받은 데이터를 통해 rendering 하도록 수정.
    * Add, Edit, Delete 시 서버로부터의 response 를 Activity 객체로 전달하여 출력하도록 수정.

--- FE/src/ts/common/configs.ts
+++ FE/src/ts/common/configs.ts
@@ -3,3 +3,4 @@ export const INIT_DATA_URI = "api/board";
 export const EDIT_DATA_URI = "api/column/{boardKey}/card/{columnKey}";
 export const DELETE_DATA_URI = "api/column/{boardKey}/card/{columnKey}";
 export const LOGIN_URI = "api/login";
+export const ADD_URI = "api/column/{boardKey}/card"
