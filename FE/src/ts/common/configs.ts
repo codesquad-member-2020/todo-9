19b4710c38e1be322792948ab188a222b6707592
choisohyun <sohyun4283@naver.com>
2020-04-16 16:29:41 +0900

    [#140] feat: 카드 이동할 때의 URI 추가
    - URI: /column/{boardKey}/card/{columnKey}

--- FE/src/ts/common/configs.ts
+++ FE/src/ts/common/configs.ts
@@ -3,4 +3,5 @@ export const INIT_DATA_URI = "api/board";
 export const EDIT_DATA_URI = "api/column/{boardKey}/card/{columnKey}";
 export const DELETE_DATA_URI = "api/column/{boardKey}/card/{columnKey}";
 export const LOGIN_URI = "api/login";
-export const ADD_URI = "api/column/{boardKey}/card"
+export const ADD_URI = "api/column/{boardKey}/card";
+export const MOVE_URI = "api/column/{boardKey}/card/{columnKey}";
