eefa7d1edd283169ddcbcc6d953bf350d72011ea
IdionKim <ksundong@gmail.com>
2020-04-16 17:13:12 +0900

    [#146] feature: 젤로가 요청하신 Card가 더 많은 정보를 담도록 기능 추가
    이제 Card가 유저정보를 담고 있게됩니다.
    Close #146 Issue.

--- BE/src/main/java/kr/codesquad/todo9/api/TodoAPIController.java
+++ BE/src/main/java/kr/codesquad/todo9/api/TodoAPIController.java
@@ -3,9 +3,11 @@ package kr.codesquad.todo9.api;
 import kr.codesquad.todo9.domain.Board;
 import kr.codesquad.todo9.domain.Column;
 import kr.codesquad.todo9.dto.BoardDTO;
+import kr.codesquad.todo9.dto.ColumnDTO;
 import kr.codesquad.todo9.dto.LogDTO;
 import kr.codesquad.todo9.error.exception.BoardNotFoundException;
 import kr.codesquad.todo9.repository.BoardRepository;
+import kr.codesquad.todo9.repository.CardRepository;
 import kr.codesquad.todo9.repository.LogRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,10 +26,14 @@ public class TodoAPIController {
 
     private final BoardRepository boardRepository;
     private final LogRepository logRepository;
+    private final CardRepository cardRepository;
 
-    public TodoAPIController(BoardRepository boardRepository, LogRepository logRepository) {
+    public TodoAPIController(BoardRepository boardRepository,
+                             LogRepository logRepository,
+                             CardRepository cardRepository) {
         this.boardRepository = boardRepository;
         this.logRepository = logRepository;
+        this.cardRepository = cardRepository;
     }
 
     @GetMapping("/board/{boardId}/column/list")
@@ -44,8 +51,14 @@ public class TodoAPIController {
     @GetMapping("/board/{boardId}")
     public BoardDTO showBoard(@PathVariable Long boardId) {
         Board board = boardRepository.findById(boardId).orElseThrow(BoardNotFoundException::new).sortBoard();
+        List<ColumnDTO> columns = new ArrayList<>();
+        for (Column column : board.getColumns()) {
+            ColumnDTO columnDTO = new ColumnDTO(column);
+            columnDTO.setCards(cardRepository.getCardList(columnDTO.getId()));
+            columns.add(columnDTO);
+        }
         List<LogDTO> logs = logRepository.getLogDTOList(defaultBoardId);
-        BoardDTO boardDTO = new BoardDTO(board, logs);
+        BoardDTO boardDTO = new BoardDTO(board, columns, logs);
         log.debug("boardDTO: {}", boardDTO);
         return boardDTO;
     }
